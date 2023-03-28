4cc2169d302d60c5a10d135ab11639ec1d58796f
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-03-28 18:44:04 -0500

    Complete Update Product

--- EntityMethods/ProductMethods.cs
+++ EntityMethods/ProductMethods.cs
@@ -1,4 +1,5 @@
-﻿using WebApplication1.Models;
+﻿using System;
+using WebApplication1.Models;
 
 namespace WebApplication1.EntityMethods
 {
@@ -35,6 +36,66 @@ namespace WebApplication1.EntityMethods
             db.Products.Remove(product);
             db.SaveChanges();
             return Results.NoContent();
+        }
+
+        public static IResult UpdateProduct(AdventureWorksLt2019Context db, int id, Product inputProduct)
+        {
+            Product productToUpdate = db.Products.Find(id);
+
+            // if product id is not found create a new product, otherwise update found product
+            if (productToUpdate == null)
+            {
+                // define new product
+                var newProduct = db.Products.Add(new Product
+                {
+                    Name = inputProduct.Name ?? string.Empty,
+                    ProductNumber = inputProduct.ProductNumber ?? string.Empty,
+                    StandardCost = inputProduct.StandardCost != 0 ? inputProduct.StandardCost : 0,
+                    ListPrice = inputProduct.ListPrice != 0 ? inputProduct.ListPrice : 0,
+                    SellStartDate = inputProduct.SellStartDate != new DateTime() ? inputProduct.SellStartDate : new DateTime(),
+                    Rowguid = Guid.NewGuid(),
+                    ModifiedDate = DateTime.Now
+                });
+
+                db.SaveChangesAsync();
+                return Results.Ok(newProduct.Entity);
+            } 
+            else
+            {
+                // do checks for all fields except id and rowid
+                if (!String.IsNullOrEmpty(inputProduct.Name))
+                    productToUpdate.Name = inputProduct.Name;
+            
+                if (!String.IsNullOrEmpty(inputProduct.ProductNumber))
+                    productToUpdate.ProductNumber = inputProduct.ProductNumber;
+                
+                if (!String.IsNullOrEmpty(inputProduct.Color))
+                    productToUpdate.Color = inputProduct.Color;
+            
+                if (inputProduct.StandardCost != 0)
+                    productToUpdate.StandardCost = inputProduct.StandardCost;
+            
+                if (inputProduct.ListPrice != 0)
+                    productToUpdate.ListPrice = inputProduct.ListPrice;
+                
+                if (!String.IsNullOrEmpty(inputProduct.Size))
+                    productToUpdate.Size = inputProduct.Size;
+
+                if (inputProduct.Weight != 0 && inputProduct.Weight != null)
+                    productToUpdate.Weight = inputProduct.Weight;
+                
+                if (inputProduct.SellStartDate != null && inputProduct.SellStartDate != new DateTime())
+                    productToUpdate.SellStartDate = inputProduct.SellStartDate;
+                
+                if (inputProduct.SellEndDate != null && inputProduct.SellEndDate != new DateTime())
+                    productToUpdate.SellEndDate = inputProduct.SellEndDate;
+            
+                // by default set modified date to DateTime.Now
+                productToUpdate.ModifiedDate = DateTime.Now;
+
+                db.SaveChangesAsync();
+                return Results.Ok(productToUpdate);
+            }
         }
     }
 }
