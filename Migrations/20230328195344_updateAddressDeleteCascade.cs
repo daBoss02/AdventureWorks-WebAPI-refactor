65ddbe0667c2242cb65cfa433df214dc2caabcb0
Mako <enderbrine2003@gmail.com>
2023-03-28 14:55:35 -0500

    Updated to cascade, delete functionality

---
+++ Migrations/20230328195344_updateAddressDeleteCascade.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAddressDeleteCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAddress_Address_AddressID",
+                schema: "SalesLT",
+                table: "CustomerAddress");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAddress_Customer_CustomerID",
+                schema: "SalesLT",
+                table: "CustomerAddress");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAddress_Address_AddressID",
+                schema: "SalesLT",
+                table: "CustomerAddress",
+                column: "AddressID",
+                principalSchema: "SalesLT",
+                principalTable: "Address",
+                principalColumn: "AddressID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAddress_Customer_CustomerID",
+                schema: "SalesLT",
+                table: "CustomerAddress",
+                column: "CustomerID",
+                principalSchema: "SalesLT",
+                principalTable: "Customer",
+                principalColumn: "CustomerID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAddress_Address_AddressID",
+                schema: "SalesLT",
+                table: "CustomerAddress");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAddress_Customer_CustomerID",
+                schema: "SalesLT",
+                table: "CustomerAddress");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAddress_Address_AddressID",
+                schema: "SalesLT",
+                table: "CustomerAddress",
+                column: "AddressID",
+                principalSchema: "SalesLT",
+                principalTable: "Address",
+                principalColumn: "AddressID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAddress_Customer_CustomerID",
+                schema: "SalesLT",
+                table: "CustomerAddress",
+                column: "CustomerID",
+                principalSchema: "SalesLT",
+                principalTable: "Customer",
+                principalColumn: "CustomerID");
+        }
+    }
+}
